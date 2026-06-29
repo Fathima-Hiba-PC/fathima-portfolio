42d6030285090b43b8a53392d8077c9fad8466f5
Fathima-Hiba-PC <fathimahibapc@gmail.com>
2026-06-29 18:33:24 +0530

    Updated portfolio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter,HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 
